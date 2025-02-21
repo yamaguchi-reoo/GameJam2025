034787673540bd140449b3d395ea87118dc1c01f
山口麗王 <reo-yamaguchi-k22h@stu.kbc.ac.jp>
2025-02-21 11:13:58 +0900

    override

--- GameJam2025-h/GameJam2025-h/Object/Item/ItemBase.h
+++ GameJam2025-h/GameJam2025-h/Object/Item/ItemBase.h
@@ -13,17 +13,17 @@ public:
     //初期化処理
     // _location 初期位置
     // _box_size 当たり判定のサイズ
-    virtual void Initialize(Vector2D _location, Vector2D _box_size);
+    virtual void Initialize(Vector2D _location, Vector2D _box_size)override;
 
     //更新処理（オーバーライドして使用）
-    virtual void Update();
+    virtual void Update()override;
 
     //描画処理（オーバーライドして使用）
-    virtual void Draw() const;
+    virtual void Draw() const override;
 
     //終了処理（オーバーライドして使用）
-    virtual void Finalize();
+    virtual void Finalize()override;
 
-    virtual void OnHitCollision(ObjectBase* hit_object);
+    virtual void OnHitCollision(ObjectBase* hit_object)override;
 };
 
