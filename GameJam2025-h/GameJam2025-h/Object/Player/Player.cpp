0b40abe4e18ed76e4c8b9fb60e36ba3a71baa099
isisokosyuntarou <syuntaro-ishisoko-k24h@stu.kbc.ac.jp>
2025-02-21 13:48:45 +0900

    playerClass追加

---
+++ GameJam2025-h/GameJam2025-h/Object/Player/Player.cpp
@@ -0,0 +1,29 @@
+#include "Player.h"
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Initialize(Vector2D _location, Vector2D _box_size)
+{
+}
+
+void Player::Update()
+{
+}
+
+void Player::Draw() const
+{
+}
+
+void Player::Finalize()
+{
+}
+
+void Player::OnHitCollision(ObjectBase* hit_object)
+{
+}
