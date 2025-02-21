4eca8df5a7411a6ff3174ee9aec5ef045ccbc5d6
yasutokuniyosi <138412658+yasutokuniyosi@users.noreply.github.com>
2025-02-21 11:36:20 +0900

    Enemy

---
+++ GameJam2025-h/GameJam2025-h/Object/Enemy/EnemyBase.cpp
@@ -0,0 +1,29 @@
+#include "EnemyBase.h"
+
+EnemyBase::EnemyBase()
+{
+}
+
+EnemyBase::~EnemyBase()
+{
+}
+
+void EnemyBase::Initialize(Vector2D _location, Vector2D _box_size)
+{
+}
+
+void EnemyBase::Update()
+{
+}
+
+void EnemyBase::Draw() const
+{
+}
+
+void EnemyBase::Finalize()
+{
+}
+
+void EnemyBase::OnHitCollision(ObjectBase* hit_object)
+{
+}
