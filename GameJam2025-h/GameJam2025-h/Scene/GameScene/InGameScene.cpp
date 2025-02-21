07ebca893a0554fb77758fa7cd38e360b57d5b7c
GG葵 <138738983+OoshiroAoi0112@users.noreply.github.com>
2025-02-21 13:41:55 +0900

    敵の生成(仮)、の追加

--- GameJam2025-h/GameJam2025-h/Scene/GameScene/InGameScene.cpp
+++ GameJam2025-h/GameJam2025-h/Scene/GameScene/InGameScene.cpp
@@ -1,6 +1,7 @@
 #include "InGameScene.h"
 #include <DxLib.h>
 #include "../../Object/Item/Apple.h"
+#include "../../Object/Enemy/WeekEnemy.h"
 #include "../../common.h"
 
 InGameScene::InGameScene()//この関数の後ろに定義した変数を連ねて書く（例 : InGameScene() : a()）
@@ -17,6 +18,7 @@ void InGameScene::Initialize()
 {
 	//ここで変数の初期化して（例：a = 0;）
 	CreateObject<Apple>(Vector2D(30.0f, 40.0f), Vector2D(64.0f));
+	CreateObject<WeekEnemy>(Vector2D(1000.0f, 640.0f), Vector2D(64.0f));
 
 	time_count = 0;
 	time_limit = 60;
@@ -37,10 +39,15 @@ eSceneType InGameScene::Update()
 	//入力管理クラスのインスタンスを取得
 	InputControl* input = InputControl::GetInstance();
 
-	//Zキーが押されたらResultシーンへ遷移
+	// 制限時間が０になったらリザルトに遷移
 	if (time_limit <= 0)
 	{
 		return eSceneType::eResult;
+	}
+	//Zキーが押されたらResultシーンへ遷移
+	if (input->GetKeyDown(KEY_INPUT_Z))
+	{
+		return eSceneType::eGameMain;
 	}
 
 	return __super::Update();
