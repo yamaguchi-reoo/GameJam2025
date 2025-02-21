7a30812864992ae69f06b05050acbfe1bcc838b4
yasutokuniyosi <138412658+yasutokuniyosi@users.noreply.github.com>
2025-02-21 16:00:40 +0900

    Hp start

--- GameJam2025-h/GameJam2025-h/Object/Enemy/WeekEnemy.cpp
+++ GameJam2025-h/GameJam2025-h/Object/Enemy/WeekEnemy.cpp
@@ -13,7 +13,7 @@ WeekEnemy::~WeekEnemy()
 void WeekEnemy::Initialize(Vector2D _location, Vector2D _box_size)
 {
 	__super::Initialize(location, _box_size);
-	location.x -= 950;
+	location.x -= 500;
 }
 
 void WeekEnemy::Update()
@@ -24,6 +24,7 @@ void WeekEnemy::Update()
 void WeekEnemy::Draw() const
 {
 	__super::Draw();
+	DrawBox(location.x+90,location.y+30,location.x + 90 + Hp*20,location.y+50,GetColor(255,255,255),true);
 }
 
 void WeekEnemy::Finalize()
@@ -33,7 +34,7 @@ void WeekEnemy::Finalize()
 void WeekEnemy::OnHitCollision(ObjectBase* hit_object)
 {
 	Hp -= 1;
-
+	
 	if (Hp == 0)
 	{
 		delete_flg = true;
